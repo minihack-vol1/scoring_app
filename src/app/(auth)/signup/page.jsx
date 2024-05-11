0b69c4f5863936a90a549ad732d81a296d024a54
yotu <114195789+ulxsth@users.noreply.github.com>
2024-05-11 17:08:21 +0900

    upd: (wip)アカウント作成画面実装

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,13 +1,37 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
+import useAuth from "@/src/hooks/useAuth"
+import {useForm} from "react-hook-form"
+import { useRouter } from 'next/router'
 
 const page = () => {
+  const router = useRouter()
+  const { signUp } = useAuth()
+  const {
+    register,
+    handleSubmit,
+    formState: {errors}
+  } = useForm()
+
+  // submit handler
+  const doRegister = async (formData) => {
+    await signUp({ email: formData.email, password: formData.password })
+    router.push("/")
+  }
+
   return (
     <>
-      <h1>アカウント作成</h1>
-      {/* TODO: Clerk導入 */}
-      <Link href="/dashboard">アカウント作成する</Link>
-      <Link href="/signin">サインインする</Link>
+      {/* TODO: validation */}
+      <form onSubmit={handleSubmit(onRegister)}>
+        <label htmlFor="email">メールアドレス</label>
+        <input type="email" id="email" {...register("email", { required: true })} />
+
+        <label htmlFor="password">パスワード</label>
+        <input type="password" id="password" {...register("password", { required: true })} />
+
+        <input type="submit" />
+      </form>
     </>
   )
 }

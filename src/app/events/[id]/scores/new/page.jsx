532b0f79e614c694a7a414020f80842d80bb6913
ulxsth <ulxsth@gmail.com>
2024-05-18 15:34:33 +0900

    upd: itemsデータの分離

--- src/app/events/[id]/scores/new/page.jsx
+++ src/app/events/[id]/scores/new/page.jsx
@@ -6,15 +6,38 @@ import React, { useState } from 'react'
 import Board from './Board';
 
 const page = () => {
+  // TODO: チーム一覧情報の取得
+  const teams = [
+    "ねずみ",
+    "うし",
+    "とら",
+    "うさぎ",
+    "りゅう",
+    "へび",
+    "うま",
+    "ひつじ",
+    "さる",
+    "とり",
+    "いぬ",
+    "いのしし",
+  ]
+
+  // TODO: 採点基準一覧の取得
+  const criteria = [
+    { id: "strength", name: "つよさ" },
+    { id: "coolness", name: "かっこよさ" },
+    { id: "cuteness", name: "かわいさ" },
+    { id: "awesomeness", name: "ヤバさ" },
+    { id: "funniness", name: "おもしろさ" },
+  ]
+
   return (
     <>
       <h1>採点する</h1>
       <div className="boards">
-        <Board name="つよさ" />
-        <Board name="かっこよさ" />
-        <Board name="かわいさ" />
-        <Board name="ヤバさ" />
-        <Board name="おもしろさ" />
+        {criteria.map((criterion) => (
+          <Board key={criterion.id} name={criterion.name} items={teams} />
+        ))}
       </div>
     </>
   );

df6b35141227248ad0c33c1d32b3fe02ae70e3e2
ulxsth <ulxsth@gmail.com>
2024-05-19 08:04:36 +0900

    migrate

---
+++ prisma/migrations/20240518230240_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdBy" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);

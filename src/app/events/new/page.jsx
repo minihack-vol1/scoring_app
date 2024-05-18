cce216a6a562094400dd949d3df96760cb97e468
ulxsth <ulxsth@gmail.com>
2024-05-18 22:38:35 +0900

    upd: イベント作成フォームの設置

--- src/app/events/new/page.jsx
+++ src/app/events/new/page.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import Link from 'next/link'
 
 const page = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+  }
+
   return (
     <>
       <h1>イベントを作成する</h1>
-
-      {/* TODO: form作成 */}
-      <Link href="/events/1">イベント作成</Link>
+      <form method='post' onSubmit={handleSubmit}>
+        <label htmlFor="name">イベント名</label>
+        <input type="text" id="name" name="name" required />
+        <button type="submit">作成</button>
+      </form>
     </>
   )
 }

505b64ee7279b48ac696731f367c42509558f82e
ulxsth <ulxsth@gmail.com>
2024-05-19 14:20:20 +0900

    upd: /api/events#GET を実装

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,6 +1,19 @@
 import prisma from "@/src/utils/prisma";
 import { NextResponse } from "next/server";
 
+export const GET = async (req, res) => {
+  try {
+    prisma.$connect();
+    const events = await prisma.event.findMany();
+    prisma.$disconnect();
+
+    return NextResponse.json(events, { status: 200 });
+  } catch (e) {
+    console.error("Error:", e);
+    return NextResponse.json({ status: 500 });
+  }
+}
+
 export const POST = async (req, res) => {
   const { name, userId: createdBy } = await req.json();
 

886cbf74161b6338a299e5cfe667d91beccc3c37
ulxsth <ulxsth@gmail.com>
2024-05-19 12:54:05 +0900

    refactor

--- src/app/events/new/page.jsx
+++ src/app/events/new/page.jsx
@@ -1,48 +1,42 @@
-"use client"
+"use client";
 
-import React from 'react'
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
+import React from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
   const router = useRouter();
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    axios.get("/api/user")
-      .then((res) => {
-        const { userId } = res.data
-        const name = e.target.name.value
+    try {
+      const res = await axios.get("/api/user");
+      const { userId } = res.data;
+      const name = e.target.name.value;
 
-        axios.post("/api/events", { name, userId })
-          .then((res2) => {
-            alert("イベントを作成しました")
+      const res2 = await axios.post("/api/events", { name, userId });
+      alert("イベントを作成しました");
 
-            const { lastInsertId } = res2.data
-            router.push(`/events/${lastInsertId}`)
-          })
-          .catch((err) => {
-            alert("エラーが発生しました")
-            console.error(err);
-          })
-      })
-      .catch((err) => {
-        alert("エラーが発生しました")
-        console.error(err);
-      })
-  }
+      const { lastInsertId } = res2.data;
+      router.push(`/events/${lastInsertId}`);
+
+    } catch (err) {
+      alert("エラーが発生しました");
+      console.error(err);
+    }
+  };
 
   return (
     <>
       <h1>イベントを作成する</h1>
-      <form method='post' onSubmit={handleSubmit}>
+      <form method="post" onSubmit={handleSubmit}>
         <label htmlFor="name">イベント名</label>
         <input type="text" id="name" name="name" required />
         <button type="submit">作成</button>
       </form>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
